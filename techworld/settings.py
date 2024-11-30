92d980fa53c29d61c7fd7b05af12e1894de257c0
MelkorDev <bahamondejosepablo@gmail.com>
2024-11-30 18:15:18 -0300

    configuracion de prueba hosts

--- techworld/settings.py
+++ techworld/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') =="True"
 
-ALLOWED_HOSTS = ['https://reforzamiento.onrender.com/','localhost']
+ALLOWED_HOSTS = ['*','localhost']
 
 
 # Application definition
