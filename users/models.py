6787bc7ec26b6be40f1e8079c69aef1f32c5999b
MelkorDev <bahamondejosepablo@gmail.com>
2024-11-30 11:26:00 -0300

    Agregar Modelo User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    ROLE_CHOICES = [
+        ("student", "Estudiante"),
+        ("instructor", "Instructor"),
+    ]
+
+    # Campos principales
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    # Campos adicionales
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="student")
+    date_of_birth = models.DateField(null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = "users"
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
