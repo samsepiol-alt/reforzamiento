9d14699bf4ed8deb715250a275b003d81cb9d702
MelkorDev <bahamondejosepablo@gmail.com>
2024-11-30 19:03:38 -0300

    prueba 2 hosts

--- techworld/settings.py
+++ techworld/settings.py
@@ -17,7 +17,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') =="True"
 
-ALLOWED_HOSTS = ['*','localhost']
+ALLOWED_HOSTS = ['localhost']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
