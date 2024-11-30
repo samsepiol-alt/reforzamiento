cb26bc58cd0c9fccf28e896a25c1a7fa55d5af2d
MelkorDev <bahamondejosepablo@gmail.com>
2024-11-30 18:42:44 -0300

    agregar superuser al build

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+#cargar superusuario
+python manage.py loaddata users/fixtures/super.json
